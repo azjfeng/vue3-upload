2ed55cfa6bd8b707af9b7f458eae489dc61b666e
azjfeng <1658744705@qq.com>
2020-11-23 11:49:38 +0800

    Update Index.vue

--- public/src/components/Index.vue
+++ public/src/components/Index.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="index">
-        <input type="file" class="upload_file">
+          <input
+            type="file"
+            class="upload_file"
+            @change="uploadFile($event)"
+        >
     </div>
 </template>
 
@@ -12,16 +16,32 @@
             }
         },
         created() {
-            console.log(window.location)
-            var origin  = window.location.origin;
-           $.ajax({
-               type:'post',
-               url:origin+"/upload",
-               success:function(res){
-                   console.log(res)
-               }
-           })
         },
+        methods: {
+            uploadFile: function(e) {
+              let origin = window.location.origin;
+              let fileName = e.target.files[0].name;
+              let file = e.target.files[0];
+              let formData = new FormData();
+              formData.append("file", file);
+              formData.append("fileName", fileName);
+              console.log(formData)
+              $.ajax({
+                url: origin+"/upload",
+                type: "post",
+                data: formData,
+                dataType: "json",
+                processData: false, // 告诉jQuery不要去处理发送的数据
+                contentType: false, // 告诉jQuery不要去设置Content-Type请求头
+                success: function(res) {
+                  console.log(res);
+                },
+                fail:function(err){
+                    console.log(err)
+                }
+              });
+            }
+  }
     }
 </script>
 
